c90fec05d6bdf9a6ad5d9ce03e77d0b5427c7054
rcskinner <ryan.skinner@datadoghq.com>
2025-07-22 15:05:17 -0600

    Basic Application Created

---
+++ test-app/test-app/test_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  test_appApp.swift
+//  test-app
+//
+//  Created by Ryan Skinner on 7/22/25.
+//
+
+import SwiftUI
+
+@main
+struct test_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
